7a4532712689b859929ae04355a74e136297b990
bea <b.pacheco@alumnos.urjc.es>
2017-10-12 14:18:20 +0200

    Me escribe un listado ordenado de etiquetas y atributos con su valor, ademas de crearme el fichero que pasa a json. Pero solo me sirve si le paso karaoke.smil. Tengo que corregirlo para que me valga para cualquier nombre de archivo

--- karaoke.py
+++ karaoke.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import json
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 from smallsmilhandler import SmallSMILHandler
@@ -12,15 +13,21 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         sys.exit("Usage: python3 karaoke.py file.smil")
 
+#Inicializa
     parser = make_parser()  # Crea el parser (leer linea a linea...)
     cHandler = SmallSMILHandler()  # Crea el handler (para manejar el xml)
     parser.setContentHandler(cHandler)  # Relaciona el parser con el handler
     parser.parse(open(sys.argv[1]))
-    
     lista = cHandler.get_tags()
-    
+
+#Escribe
     for etiqueta in lista:
-        print(etiqueta['name'], '\t')
+        nombre = etiqueta['name']
         for atributo in etiqueta:
             if etiqueta[atributo] != "" and atributo != 'name':
-                print(atributo, ' = ', etiqueta[atributo])
+                nombre += '\t' + atributo + '="' + etiqueta[atributo] + '"'
+        print(nombre)
+        nombre = ""
+
+#Crear el archivo json
+    json.dump([lista], open("karaoke.json", "w"))
