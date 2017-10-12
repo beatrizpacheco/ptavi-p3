7498f4f0354ffc3168b9c70d1125a41fdf317d24
bea <b.pacheco@alumnos.urjc.es>
2017-10-12 23:53:19 +0200

    ¡¡¡Casi acabadogit add karaoke.py! Falta el punto 6 de la práctica, y cambiar algunos nombres de variables para que se entienda mejor

--- karaoke.py
+++ karaoke.py
@@ -9,28 +9,44 @@ from smallsmilhandler import SmallSMILHandler
 from urllib.request import urlretrieve
 
 
+class KaraokeLocal(SmallSMILHandler):
+
+    def __init__(self, fichero):
+        parser = make_parser()
+        cHandler = SmallSMILHandler()
+        parser.setContentHandler(cHandler)
+        parser.parse(open(fichero))
+        self.lista = cHandler.get_tags()
+
+    def __str__ (self):
+        str_lista = ""
+        for etiqueta in self.lista:
+            str_lista += etiqueta['name']
+            for atributo in etiqueta:
+                if etiqueta[atributo] != "" and atributo != 'name':
+                    str_lista += '\t' + atributo + '="' + etiqueta[atributo] + '"'
+            str_lista += '\n'
+        return(str_lista)
+
+    def to_json(self, fich_smil, fich_json = None):
+        if fich_json == None:
+            fich_json = fich_smil.split('.')[0] + '.json'
+        json.dump([self.lista], open(fich_json, "w"))
+
+
+    def do_local(self):
+        for etiqueta in self.lista:
+            for atributo in etiqueta:
+                if etiqueta[atributo][0:7] == "http://":
+                    urlretrieve(etiqueta[atributo], etiqueta[atributo].split("/")[-1])
+
 if __name__ == "__main__":
 
     if len(sys.argv) != 2:
         sys.exit("Usage: python3 karaoke.py file.smil")
-
-#Inicializa
-    parser = make_parser()  # Crea el parser (leer linea a linea...)
-    cHandler = SmallSMILHandler()  # Crea el handler (para manejar el xml)
-    parser.setContentHandler(cHandler)  # Relaciona el parser con el handler
-    parser.parse(open(sys.argv[1]))
-    lista = cHandler.get_tags()
-
-#Escribe y descarga lo remoto
-    for etiqueta in lista:
-        nombre = etiqueta['name']
-        for atributo in etiqueta:
-            if etiqueta[atributo][0:7] == "http://":
-                urlretrieve(etiqueta[atributo], etiqueta[atributo].split("/")[-1])
-            if etiqueta[atributo] != "" and atributo != 'name':
-                nombre += '\t' + atributo + '="' + etiqueta[atributo] + '"'
-        print(nombre)
-        nombre = ""
-
-#Crear el archivo json
-    json.dump([lista], open("karaoke.json", "w"))
+    objeto = KaraokeLocal(sys.argv[1])
+    print(objeto.__str__())
+    objeto.to_json(sys.argv[1])
+    objeto.do_local()
+    objeto.to_json(sys.argv[1], 'local.json')
+    print(objeto.__str__())
